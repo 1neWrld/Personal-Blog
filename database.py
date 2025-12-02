37df9918d449d8db85ec698f2fcb9897edc51ccd
Wandipa Marema <wandipamarema@icloud.com>
2025-12-02 17:34:15 +0200

    - Removed SQLITE - Tables will now be created with PostgreSQL within Render.com

--- database.py
+++ database.py
@@ -2,15 +2,15 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker, scoped_session
+import os
 
 # Define base class
 class Base(DeclarativeBase):
     pass
 
-# Create engine and connect to SQLite database 'echo=True' see SQL queries
-import os
+# Create engine and read database url from env fallback to SQLite locally 'echo=True' see SQL queries
 db_path = os.environ.get('DATABASE_URL', 'sqlite:///blog.db')
-engine = create_engine('sqlite:///blog.db', echo=True)
+engine = create_engine(db_path, echo=True)
 
 """
 scoped_session creates a safe thread 
